fce6e03cbef21e8e448c1e1c9e7a08ac301dd200
Itzel01 <itzel.areymi.rendon@gmail.com>
2021-03-02 16:29:18 -0500

    trying to render the page

--- script.js
+++ script.js
@@ -9,8 +9,26 @@
  })
 
  function addPagehandler(e){
-     e.target.style.display = "none"
-     let form = document.createElement("form")
-     
+     e.target.style.display = "none";
+     makeForm();
+ }
 
+ function makeForm (){
+    let form = document.createElement("form");
+    let input = document.createElement("input");
+    let note = document.getElementById("add-note");
+    note.append(form, input);
+
+    form.addEventListener("submit", submitHandler)
+   // makeLinedPageWInput(form, input, note)
+ }
+
+ function submitHandler(e, input, note){
+    let p = document.createElement("p");
+    let text = input.value; 
+    console.log(input);
+    p.innerText = `${text}`;
+    note.append(p)
+    console.log(note)
+    console.log(p.innerText)
  }
