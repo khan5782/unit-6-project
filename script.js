cf24e81829f74e4db64747a6acc7657a798c69b8
Asif <kasif5782@gmail.com>
2021-03-02 20:44:37 -0500

    edit js to add entries

--- script.js
+++ script.js
@@ -6,22 +6,78 @@
  document.addEventListener("DOMContentLoaded", () => {
      let addPage = document.getElementById("add-page")
      addPage.addEventListener("click", addPagehandler)
+     let button = document.getElementById("journal-save")
+     button.addEventListener("click", saveJournal)
  })
 
  function addPagehandler(e){
-     e.target.style.display = "none";
-     makeForm();
+    e.target.style.display = "none";
+    let journalpage = document.getElementById("journal-page")
+    journalpage.style.display = "block"
  }
 
- function makeForm (){
-    let form = document.createElement("form");
-    let input = document.createElement("input");
-    let note = document.getElementById("add-note");
-    note.append(form, input);
+ function saveJournal(){
+    let journaltitle = document.getElementById("journal-title")
+    let journaldate = document.getElementById("journal-date")
+    let journalcontent = document.getElementById("journal-content")
+    
+    let journals = JSON.parse(localStorage.getItem("journals")) || []
+    journals.push({
+        journaltitle: journaltitle.value,
+        journaldate: journaldate.value,
+        journalcontent: journalcontent.value,
+        id: Date.now()
+    })
+    localStorage.setItem("journals", JSON.stringify(journals))
+    console.log(localStorage)
+    let journalpage = document.getElementById("journal-page")
+    journalpage.style.display = "none"
+    let addPage = document.getElementById("add-page")
+    addPage.style.display = "block"
+    journaltitle.value = ""
+    journaldate.value = ""
+    journalcontent.value = ""
 
-    form.addEventListener("submit", submitHandler)
-   // makeLinedPageWInput(form, input, note)
+    // start to populate ul
+    journals = JSON.parse(localStorage.getItem("journals"))
+    let journallist = document.getElementById("journal-list")
+    journals.forEach(journal => {
+        let journalitem = document.createElement("li")
+        journalitem.innerText = journal.journaltitle
+        journalitem.id = journal.id
+        journalitem.addEventListener("mouseover", function(e) {
+            journalitem.style.background = 'blue'
+        })
+        journalitem.addEventListener("mouseleave", function(e) {
+            journalitem.style.background = 'white'
+        })
+        journalitem.addEventListener("click", function(e) {
+            e.preventDefault()
+            let journaldate = document.createElement("p")
+            let journalcontent = document.createElement("p")
+            journaldate.innerText = journal.journaldate
+            journalcontent.innerText = journal.journalcontent
+            journalitem.append(journalcontent, journaldate)
+            
+        })
+        
+
+        journallist.append(journalitem)
+    })
+
  }
+//  function makeForm (){
+//     let date = document.createElement("input");
+//     let inputtitle = document.createElement("input");
+//     let button = document.createElement("button")
+//     let textinput = document.createElement("textarea")
+//     let note = document.getElementById("add-note");
+//     button.innerText = "Save Journal"
+//     date.type = "date"
+//     textinput.cols = "30"
+//     textinput.rows = "30"
+//     note.append( inputtitle, date, textinput, button);
+//  }
 
  function submitHandler(e, input, note){
     let p = document.createElement("p");
