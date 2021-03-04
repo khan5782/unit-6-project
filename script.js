99cd9c6beeedd2aa9e88357524b00835196521bc
Itzel01 <itzel.areymi.rendon@gmail.com>
2021-03-04 15:49:22 -0500

    I broke our code

--- script.js
+++ script.js
@@ -2,90 +2,120 @@
      let addPage = document.getElementById("add-page")
      addPage.addEventListener("click", addPagehandler)
      let button = document.getElementById("journal-save")
-     button.addEventListener("click", saveJournal)
+     button.addEventListener("click", eventHandler)
  })
 
+
+
+//ADD PLANNER 
+
+
+
+
+
+
+
+
+//ADD JOURNAL 
  function addPagehandler(e){
     let pagecontainer = document.getElementById("add-note")
     pagecontainer.style.display = "none"
     let journalpage = document.getElementById("journal-page")
     journalpage.style.display = "block"
+    console.log("addPageHandler")
+ }
+ function eventHandler (e){
+    e.preventDefault();
+    // grabbing input ele for each journal
+    let journaltitle = document.getElementById("journal-title");
+    let journaldate = document.getElementById("journal-date");
+    let journalcontent = document.getElementById("journal-content");
+    updateLocalStorage(journaltitle, journaldate, journalcontent)
+    console.log("eventHandler")
  }
 
- function saveJournal(e){
-    e.preventDefault()
-    // grabbing input ele for each journal
-    let journaltitle = document.getElementById("journal-title")
-    let journaldate = document.getElementById("journal-date")
-    let journalcontent = document.getElementById("journal-content")
-    // get local journals if they are there else set [] of journals
-    let journals = JSON.parse(localStorage.getItem("journals")) || {}
-    // add journal to local storage
-    const uniid = Date.now()
-    journals[uniid]={
-        journaltitle: journaltitle.value,
-        journaldate: journaldate.value,
-        journalcontent: journalcontent.value,
-        id: uniid
-    }
-    localStorage.setItem("journals", JSON.stringify(journals))
+ function updateLocalStorage(title, date, content){
+     // get local journals if they are there else set [] of journals
+     let journals = JSON.parse(localStorage.getItem("journals")) || {}
+     // add journal to local storage
+     const uniid = Date.now()
+     journals[uniid]={
+         title: title.value,
+         date: date.value,
+         content: content.value,
+         id: uniid
+     }
+     localStorage.setItem("journals", JSON.stringify(journals))
+
+     manipulationWithDisplay(title, date, content)
+     console.log("updateLocalStorage")
+ }
+
+ function manipulationWithDisplay(title, date, content){
     // hiding journal entry page and showing home page 
     let journalpage = document.getElementById("journal-page")
     journalpage.style.display = "none"
     let addnote = document.getElementById("add-note")
     addnote.style.display = "block"
     // setting input values to ""
-    journaltitle.value = ""
-    journaldate.value = ""
-    journalcontent.value = ""
-    createJournalList()
+    title.value = ""
+    date.value = ""
+    content.value = ""
+    getItemToAdd()
+    console.log("manipulationWithDisplay")
  }
 
- function createJournalList(){
-     // read save journal in local storage 
+ function getItemToAdd(){
+    // read save journal in local storage 
     let journals = JSON.parse(localStorage.getItem("journals"))
     let journallist = document.getElementById("journal-list")
     journallist.innerHTML = ''
-    // for each journal we want to add a journal to li
     const journalkeys = Object.keys(journals)
-    journalkeys.forEach(key => {
-        let journal = journals[key]
-        let journalitem = document.createElement("li")
-        journalitem.innerText = journal.journaltitle
-        journalitem.id = journal.id
+    journalkeys.forEach(createJournalList)
+    console.log("getItemToAdd")
+ }
 
-        // create el to show and hide each el
-        let journalcontainer = document.createElement("div")
-        journalcontainer.id = `container-${journal.id}`
-        journalcontainer.className = "innerDiv"
+ function createJournalList(){
+    // debugger
+    // for each journal we want to add a journal to li
+    let journal = journals[key]
+    let journalitem = document.createElement("li")
+    journalitem.innerText = journal.title
+    journalitem.id = journal.id
 
-        let journaldate = document.createElement("p")
-        let journalcontent = document.createElement("p")
-        let deletebutton = document.createElement("button")
-        deletebutton.innerText = "Delete Journal"
-        journaldate.innerText = journal.journaldate
-        journalcontent.innerText = journal.journalcontent
+    // create el to show and hide each el
+    let journalcontainer = document.createElement("div")
+    journalcontainer.id = `container-${journal.id}`
+    journalcontainer.className = "innerDiv"
+    let innerDivDate = document.createElement("p")
+    let innerDivContent = document.createElement("p")
+    let deletebutton = document.createElement("button")
+    deletebutton.innerText = "Delete Journal"
+    innerDivDate.innerText = journal.date
+    innerDivContent.innerText = journal.content
 
-        // adding to journal container
-        journalcontainer.append(journalcontent, journaldate, deletebutton)
-        journalitem.append(journalcontainer)
-        journalcontainer.style.display = "none"
-       
-        // add click even to hide and show container 
-        journalitem.addEventListener("click", function(e) {
-            e.preventDefault()
-            let journalvisb = journalcontainer.style.display
-            if(journalvisb == "none"){
-                journalcontainer.style.display = "block"
-            } else {
-                journalcontainer.style.display = "none"
-            }
-        })
-        deletebutton.addEventListener("click", () => {deleteJournal(journal.id)})
-        journallist.append(journalitem)
+    // adding to journal container
+    journalcontainer.append(content, date, deletebutton)
+    journalitem.append(journalcontainer)
+    journalcontainer.style.display = "none"
+console.log("createJournalList");
+    behaviorOnJItem(journalitem)
+ }
 
+ function behaviorOnJItem(journalitem){ 
+    // add click event to hide and show container 
+    journalitem.addEventListener("click", function(e) {
+        e.preventDefault()
+        let journalvisb = journalcontainer.style.display
+        if(journalvisb == "none"){
+            journalcontainer.style.display = "block"
+        } else {
+            journalcontainer.style.display = "none"
+        }
     })
-    
+        deletebutton.addEventListener("click", () => {deleteJournal(journal.id)})
+        journallist.append(journalitem) 
+        console.log("behaviorOnJItem")
  }
 
  function deleteJournal(id){
@@ -106,5 +136,4 @@
     console.log(note)
     console.log(p.innerText)
  }
-
  
